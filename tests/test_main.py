f0c4373347d591f0aef117759a220319949da25c
buonassisi <17222034+buonassisi@users.noreply.github.com>
2025-05-21 21:04:09 -0400

    Changes to be committed: 	new file:   .vscode/settings.json 	new file:   tests/__pycache__/test_design.cpython-311-pytest-7.4.0.pyc 	new file:   tests/__pycache__/test_main.cpython-311-pytest-7.4.0.pyc 	renamed:    tests/test_design -> tests/test_design.py 	renamed:    tests/test_main -> tests/test_main.py

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,3 +1,7 @@
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
 import numpy as np
 import pandas as pd
 from src.utils import x_normalizer, x_denormalizer
